146eb0d26d2d71dd7ac302787f2bcfee76e1b1ec
Wilson Stiven Salgado Blanco <wilson.salgado@tecnofactory.com.co>
2026-07-13 11:52:58 -0500

    Desarrollo de Front Blazor con SignalR

---
+++ BLAZOR/EpsLaNuestra.BlazorApp/EpsLaNuestra.BlazorApp/Hubs/AdmisionHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace EpsLaNuestra.BlazorApp.Hubs;
+
+public class AdmisionHub : Hub
+{
+    public async Task NotifyNewAdmission(string patientNumber, int copaymentAmount)
+    {
+        await Clients.All.SendAsync("ReceiveAdmission", patientNumber, copaymentAmount);
+    }
+}
---
+++ BLAZOR/EpsLaNuestra.BlazorApp/EpsLaNuestra.BlazorApp/Program.cs
@@ -0,0 +1,30 @@
+using EpsLaNuestra.BlazorApp.Components;
+using EpsLaNuestra.BlazorApp.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.MapHub<AdmisionHub>("/admisionHub");
+
+app.Run();
